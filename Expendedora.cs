abf5c9c70ecc14e644d972053fb8cbc5fa84a188
Alumno <Alumno@L23-2543295>
2022-08-24 13:01:04 -0500

    Agregar archivos de proyecto.

---
+++ Expendedora.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+
+namespace ExpendedoraBase
+{
+    class Expendedora
+    {
+        string marca;
+        byte temperatura;
+        string codigo;
+        float precio;
+
+        public float Precio
+        {
+            get => precio;
+            set
+            {
+                if (value >= 10 && value == 50)
+                {
+                    precio = value;
+                }
+                else
+                {
+                    precio = 10;
+                }
+            }
+        }
+
+        public Expendedora()
+        { 
+            Saludar();
+            marca = "AMS";
+            Console.WriteLine("Marca: " +marca);
+            ControlarTiempoDisplay();
+            temperatura = 14;
+            Console.WriteLine("La temperatura es : " + temperatura + "°C");//, y + hacen lo mismo;
+            ControlarTiempoDisplay();
+            codigo = MostrarCodigodeProductos();
+            ControlarTiempoDisplay();
+            MostrarPrecio(codigo);
+        }
+        public Expendedora(bool soporte)
+        {
+            Console.WriteLine("Entrando modo soporte");
+            Console.WriteLine("La temperatura actual es: ", temperatura, "°C");
+        }
+
+        void ControlarTiempoDisplay()
+        {
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+        void Saludar()
+        {
+            Console.WriteLine("Bienvenido");
+        }
+
+        
+          string MostrarCodigodeProductos()
+        {
+            Console.WriteLine("A1: Kinder Delice \t B2 Takis \t C3: Donitas");
+            Console.WriteLine("Ingresa el codigo del producto que deseas");
+            string codigo = Console.ReadLine(); // "Codigo" es una variable local
+            return codigo;
+        }
+
+        void MostrarPrecio(string codigo)
+        {
+            Precio = 0;
+            switch (codigo)
+            {
+                case "A1":
+                    Console.WriteLine("El precio es: {0} ", Precio); 
+                    break;
+
+                case "A2":
+                    Console.WriteLine("El precio es: {0} ", Precio);
+                    break;
+                case "A3":
+                    Console.WriteLine("El precio es: {0} ", Precio);
+                    break;
+                default:
+                    Console.WriteLine("Producto no disponible");
+                    break;
+
+            }
+
+        }
+
+    
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ExpendedoraBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expendedora miExpendedora = new Expendedora();
+            Console.WriteLine("¿Quieres entrar a modo soporte? (S/N)");
+            char respuesta = Convert.ToChar(Console.ReadLine());
+            if (respuesta=='S'||respuesta=='s')
+            {
+                miExpendedora = new Expendedora(true);
+            }
+            Console.ReadLine();
+        }
+    }
+}
